feef595c7df60ccdf050fde9e3ac453cfc06fae2
Xasan <ahmuratovxasan@gmail.com>
2022-10-25 07:38:37 +0500

    salom

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// let number = 12;
+// console.log(number);
+
+// const nameFirst = "Xasan";
+// console.log(nameFirst);
+
+// const m ={
+//     name: "salom",
+// };
+
+// m.name = "xayr";
+// console.log(m);
+
+// alert("Salom hasan bo'sh kelma");
+
+// const theif = {
+//     age: 26,
+//     firstName: "Xasan",
+//     lastName: "Ahmuratov",
+// };
+// console.log(theif);
+
+// const massiv = ["xasan","xusan","aziz","jasur","xoshim"];
+// console.log(massiv[0]);
+
+// const ranglar = [];
+
+// ranglar[0] = prompt("Yoqtirgan ranglarizni kiriting #1", "");
+// ranglar[1] = prompt("Yoqtirgan ranglarizni kiriting #2", "");
+// ranglar[2] = prompt("Yoqtirgan ranglarizni kiriting #3", "");
+// ranglar[3] = prompt("Yoqtirgan ranglarizni kiriting #4", "");
+
+//     console.log(typeof ranglar);
---
+++ js/js.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// const chanuel = prompt("Youtube kanalini nomini yozing!");
+
+// console.log("https://www.youtube.com/watch?v=gBzqxg-CqAU" + chanuel);
+
+const ozgaruvchi = prompt("salom dunyo!");
+
+console.log(`salom, ${ozgaruvchi}`);
